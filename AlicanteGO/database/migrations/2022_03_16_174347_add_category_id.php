c2278bc06910f4da87eaa1dd3b7949544ca847c7
ariusp <arp99@alu.ua.es>
2022-03-16 19:28:16 +0100

    Created categories migration and seeder

---
+++ AlicanteGO/database/migrations/2022_03_16_174347_add_category_id.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('establishments', function (Blueprint $table) {
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('establishments', function (Blueprint $table) {
+            $table->dropColumn('category_id');
+        });
+    }
+}
