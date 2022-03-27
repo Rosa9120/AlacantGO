0e630ad26b736519a77081fb80aca4e05c3d158b
unknown <nt33@gcloud.ua.es>
2022-03-27 13:39:22 +0200

    web.php conflicts resolved

--- AlicanteGO/routes/web.php
+++ AlicanteGO/routes/web.php
@@ -15,21 +15,6 @@ use App\Http\Controllers\BrandController;
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', function () {return view('welcome');});
-
-
-//BRAND ROUTES
-Route::get('brand', function () {return view('brand.paths_overview');});
-
-Route::get('brand/get', function () {return view('brand.getForm');}); //ACCEDEMOS AL FORMULARIO
-Route::post('brand', [BrandController::class, 'get_brand'])->name('brand.getBrand'); //ENVIAMOS LA INFORMACIÓN INTRODUCIDA DEL FORMULARIO AL CONTROLADOR
-
-Route::get('brand/set', function () {return view('brand.setForm');});
-Route::post('brand', [BrandController::class, 'set_brand'])->name('brand.setBrand');
-
-//Route::put('brand', [BrandController::class, 'form'])->name('brand.getBrand');
-=======
 Route::get('/', function () {
     return view('home');
 });
@@ -38,9 +23,25 @@ Route::get('/admin', function () {
     return view('admin');
 });
 
+/**
+ * MANAGER ROUTES
+ */
 Route::get('/managers', [App\Http\Controllers\ManagerController::class, 'index']);
 Route::delete('/managers/{manager}', [App\Http\Controllers\ManagerController::class, 'delete']);
+
+/**
+ * ESTABLISHMENT ROUTES
+ */
 Route::get('/establishments', [App\Http\Controllers\EstablishmentController::class, 'get_all']);
 Route::get('/establishment/{id}', [App\Http\Controllers\EstablishmentController::class, 'get_establishment']);
 Route::get('/establishmentadd', [App\Http\Controllers\EstablishmentController::class, 'create_establishment']);
->>>>>>> ea12c157931d50a392360c1955a836dfaec68c7f
+
+/**
+ * BRAND ROUTES
+ */
+//Route::put('brand', [BrandController::class, 'form'])->name('brand.getBrand');
+Route::get('brand', function () {return view('brand.paths_overview');});
+Route::get('brand/get', function () {return view('brand.getForm');}); //ACCEDEMOS AL FORMULARIO
+Route::post('brand', [BrandController::class, 'get_brand'])->name('brand.getBrand'); //ENVIAMOS LA INFORMACIÓN INTRODUCIDA DEL FORMULARIO AL CONTROLADOR
+Route::get('brand/set', function () {return view('brand.setForm');});
+Route::post('brand', [BrandController::class, 'set_brand'])->name('brand.setBrand');
