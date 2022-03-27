ee83bfd7880b2347f36ffe5aa50f8c6af2f4104e
Ilya Slyusarchuk <ilyaniesvb@gmail.com>
2022-03-27 20:15:19 +0200

    Changed some styles and added single item view

--- AlicanteGO/resources/views/items/items.blade.php
+++ AlicanteGO/resources/views/items/items.blade.php
@@ -4,7 +4,7 @@
 
 <div style="display:flex;justify-content:space-around;margin-top:40px;">
     <div>
-        <h3>Total items: {{ count($items) }}</h3>
+        <h3>Total items: {{ $count }}</h3>
     </div>
     <div>
         <a class="btn btn-success" href="{{ 1+1 }}"> Create New Product</a>
@@ -26,11 +26,10 @@
         <td>{{ $item->name }}</td>
         <td>{{ $item->price }}€</td>
         <td>
+            <a class="btn btn-primary" href="{{ url("/items/" . $item->id) }}">Show</a>
+            <a class="btn btn-warning" href="#">Edit</a>
+ 
             <form action="{{ url('/items', ['id' => $item->id]) }}" method="POST">
- 
-                <a class="btn btn-primary" href="#">Show</a>
-                <a class="btn btn-warning" href="#">Edit</a>
- 
                 @csrf
                 @method('delete')
                 <input type="submit" class="btn btn-danger" value="Delete"/>
@@ -40,19 +39,16 @@
     @endforeach
 </table>
 
-{{-- <div style="display:flex; justify-content:center; padding-top:35px; height: 70vh;">
-    <div class="m-3 justify-content-center text-dark" style="overflow-y:scroll;display:inline-block; padding: 20px; background-color:rgba(255,255,255,0.7);">
-        @foreach ($items as $item)
-            <form action="{{ url('/items', ['id' => $item->id]) }}" method="post">
-                @csrf
-                @method('delete')
-                <div style="margin-top:20px;">
-                    <span>{{$item->name}}</span>
-                    <input class="btn btn-danger" type="submit" value="Delete" />
-                </div>
-            </form>
-        @endforeach
-    </div>
-</div> --}}
+{{ $items->links() }}
 
+@endsection
+
+@section('style')
+    .pagination {
+        justify-content: center;    
+    }
+
+    form {
+        display: inline-block;
+    }
 @endsection
--- AlicanteGO/routes/web.php
+++ AlicanteGO/routes/web.php
@@ -25,6 +25,7 @@ Route::get('/admin', function () {
 
 // ITEMS' ROUTES
 Route::get('/items', [App\Http\Controllers\ItemController::class, 'index']);
+Route::get('/items/{item}', [App\Http\Controllers\ItemController::class, 'show']);
 Route::delete('/items/{item}', [App\Http\Controllers\ItemController::class, 'delete']);
 
 // MANAGERS' ROUTES
@@ -32,13 +33,13 @@ Route::get('/managers', [App\Http\Controllers\ManagerController::class, 'index']
 Route::delete('/managers/{manager}', [App\Http\Controllers\ManagerController::class, 'delete']);
 
 //BRAND ROUTES
-Route::get('brand', function () {return view('brand.paths_overview');});
+Route::get('/brands', function () {return view('brand.paths_overview');});
 
-Route::get('brand/get', function () {return view('brand.getForm');}); //ACCEDEMOS AL FORMULARIO
-Route::post('brand', [BrandController::class, 'get_brand'])->name('brand.getBrand'); //ENVIAMOS LA INFORMACIÓN INTRODUCIDA DEL FORMULARIO AL CONTROLADOR
+Route::get('/brand/get', function () {return view('brand.getForm');}); //ACCEDEMOS AL FORMULARIO
+Route::post('/brandg', [BrandController::class, 'get_brand'])->name('brand.getBrand'); //ENVIAMOS LA INFORMACIÓN INTRODUCIDA DEL FORMULARIO AL CONTROLADOR
 
-Route::get('brand/set', function () {return view('brand.setForm');});
-Route::post('brand', [BrandController::class, 'set_brand'])->name('brand.setBrand');
+Route::get('/brand/set', function () {return view('brand.setForm');});
+Route::post('/brand_set', [BrandController::class, 'set_brand'])->name('brand.setBrand');
 
 // ESTABLISHMENTS' ROUTES
 Route::get('/establishments', [App\Http\Controllers\EstablishmentController::class, 'create_establishment']);
