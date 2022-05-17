f87ff9b30371d78d041487feee3eb055aa868324
VicentevonEverec <vjmr1@gcloud.ua.es>
2022-05-17 22:56:39 +0200

    Model code incorporated

--- AlicanteGO/app/Models/Item.php
+++ AlicanteGO/app/Models/Item.php
@@ -16,4 +16,39 @@ class Item extends Model
     public function establishment() {
         return $this->belongsTo(Establishment::class);
     }
+
+    static public function create($name, $price, $description, $type, $brand, $establishment)
+    {
+        $item = new Item;
+        $item->name = $name;
+        $item->price = $price;
+        $item->description = $description;
+        $item->type = $type;
+
+        if ($brand != NULL) 
+            $item->brand()->associate($brand);
+
+        else 
+            $item->establishment()->associate($establishment);
+
+        $item->save();
+
+        return $item;
+    }
+
+    static public function edit($item, $name, $price, $description, $type, $brand, $establishment)
+    {
+        $item->name = $name;
+        $item->price = $price;
+        $item->description = $description;
+        $item->type = $type;
+
+        if ($brand != NULL) 
+            $item->brand()->associate($brand);
+
+        else 
+            $item->establishment()->associate($establishment);
+
+        $item->save();
+    }
 }
