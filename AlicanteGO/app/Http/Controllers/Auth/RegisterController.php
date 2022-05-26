22774837c3e8929d20e3dff68b4620b1fbde2541
ariusp <arp99@alu.ua.es>
2022-05-26 17:23:25 +0200

    Finished Register view

--- AlicanteGO/app/Http/Controllers/Auth/RegisterController.php
+++ AlicanteGO/app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'DNI' => ['required', 'regex:/^\d{8}[A-Z]$/'],
+            'phone' => ['required', 'numeric', 'digits:9']
         ]);
     }
 
